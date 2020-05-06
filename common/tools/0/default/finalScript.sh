13dc05bacf664783f981b0a2de3e42b4bdf10c54
jilleb <jjberends@gmail.com>
2020-05-06 15:48:19 +0200

    Updated install script.

--- common/tools/0/default/finalScript.sh
+++ common/tools/0/default/finalScript.sh
@@ -8,8 +8,15 @@ mount -uw /net/mmx/mnt/app/
 
 echo "Copying MQB-coding green menu to MIB1"
 cp /net/mmx/fs/sda0/Toolbox/mqbcoding.esd /net/mmx/mnt/app/eso/hmi/engdefs/mqbcoding.esd
-cp /net/mmx/fs/sda0/Toolbox/Scripts/*.* /net/mmx/mnt/app/eso/hmi/engdefs/Scripts/
-chmod /net/mmx/bin/chmod a+rwx /net/mmx/mnt/app/eso/hmi/engdefs/Scripts/*
+cp /net/mmx/fs/sda0/Toolbox/Scripts/*.* /net/mmx/mnt/app/eso/hmi/engdefs/scripts/
+chmod /net/mmx/bin/chmod a+rwx /net/mmx/mnt/app/eso/hmi/engdefs/scripts/*
+
+
+cd /net/mmx/mnt/app/eso/bin/
+export LD_LIBRARY_PATH=/net/mmx/mnt/app/root/lib-target:/net/mmx/mnt/eso/lib:/net/mmx/mnt/app/usr/lib:/net/mmx/mnt/app/armle/lib:/net/mmx/mnt/app/armle/lib/dll:/net/mmx/mnt/app/armle/usr/lib
+export IPL_CONFIG_DIR=/etc/eso/production
+
+/net/mmx/eso/bin/dumb_persistence_writer -P 0 3221356557 01
 
 echo FinalScript finished
 
